e770e04bae60e83a5493a337c61fe3bba629a197
hari-hech-kay <48200258+hari-hech-kay@users.noreply.github.com>
2020-07-11 16:01:38 +0530

    Node http

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Hello World');
+});
+
+http.createServer(app).listen(8080, () => { console.log("Server is up and running"); });
+
+
